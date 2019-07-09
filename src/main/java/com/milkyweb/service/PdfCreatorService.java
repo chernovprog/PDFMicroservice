909722c8fa6647fbedd49ddc8b245a38894f930d
Andrey Chernov <andrii.chernov@gmail.com>
2019-07-09 20:21:10 +0300

    release v1.1 migrate from iText to Jasper

--- src/main/java/com/milkyweb/service/PdfCreatorService.java
+++ src/main/java/com/milkyweb/service/PdfCreatorService.java
@@ -1,59 +1,42 @@
 package com.milkyweb.service;
 
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfVersion;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.kernel.pdf.WriterProperties;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Cell;
-import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.element.Table;
-import com.itextpdf.layout.property.TextAlignment;
-import com.itextpdf.layout.property.VerticalAlignment;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
 import org.springframework.stereotype.Service;
 
-import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PdfCreatorService {
-    private static final String DEST = "D:/PDF/demo.pdf";
+    private static final String DEST = "demo.pdf";
+    private static final String JRXML = "report1.jrxml";
 
     public void createPdfFile() {
-        PdfWriter writer = null;
-
         try {
-            writer = new PdfWriter(DEST, new WriterProperties().setPdfVersion(PdfVersion.PDF_2_0));
-        } catch (FileNotFoundException e) {
+            JasperReport jasperReport = JasperCompileManager.compileReport(JRXML);
+            JRDataSource jrDataSource = new JREmptyDataSource();
+            Map<String, Object> parameters = getParameters();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, jrDataSource);
+            JasperExportManager.exportReportToPdfFile(jasperPrint, DEST);
+        } catch (JRException e) {
             e.printStackTrace();
         }
 
-        PdfDocument pdfDocument = new PdfDocument(writer);
-        pdfDocument.setTagged();
-        Document document = new Document(pdfDocument);
+    }
 
-        document.add(new Paragraph("Rate & Load Confirmation"));
+    private Map<String, Object> getParameters() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("title", "my title");
+        parameters.put("number", 123);
 
-
-        Table table = new Table(3);
-        Cell cell = new Cell(1, 3)
-                .setTextAlignment(TextAlignment.CENTER)
-                .add(new Paragraph("Cell with colspan 3"));
-        table.addCell(cell);
-        cell = new Cell(2, 1)
-                .add(new Paragraph("Cell with rowspan 2"))
-                .setVerticalAlignment(VerticalAlignment.MIDDLE);
-        table.addCell(cell);
-        table.addCell("Cell 1.1");
-        table.addCell(new Cell().add(new Paragraph("Cell 1.2")));
-        table.addCell(new Cell()
-                .add(new Paragraph("Cell 2.1"))
-                .setMargin(5));
-        table.addCell(new Cell()
-                .add(new Paragraph("Cell 1.2"))
-                .setPadding(5));
-        document.add(table);
-
-        document.close();
+        return parameters;
     }
 
 }
